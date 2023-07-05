15991f14aba5a26aed66615485f152bedb4d7c5a
claire-lindstrom <lindstrom.claire@gmail.com>
2023-07-05 16:48:18 -0500

    Add hamburger menu function and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Welcome to my portfolio site! Thanks for checking it out.")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
